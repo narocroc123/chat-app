abdfbeadaedc7d28a102e70e225ac41a242c3958
narocroc123 <42586341+narocroc123@users.noreply.github.com>
2019-05-19 17:08:16 +0100

    Initial Commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    appID: '3379e0ed404f34',
+    apiKey: '1c70749b5f197c54dd8782f7abf2ad787bb19dd3'
+};
+
+export default config;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {CometChat} from '@cometchat-pro/chat';
+import App from './components/App';
+import config from './config';
+    
+CometChat.init(config.appID);
+    
+ReactDOM.render(, document.getElementById('root'));
